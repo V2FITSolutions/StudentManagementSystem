97f46e12bba4cdd861419a5d8541dee451dfa1b1
V2FITSolutions <118956480+V2FITSolutions@users.noreply.github.com>
2022-11-24 16:41:25 +0800

    Update V2FSolutions - StudentManagementSystem (11-24-2022).sql

--- V2FSolutions - StudentManagementSystem (11-24-2022).sql
+++ V2FSolutions - StudentManagementSystem (11-24-2022).sql
@@ -3,6 +3,7 @@
 -- Server version:               10.4.21-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
 -- HeidiSQL Version:             11.3.0.6295
+-- Pungak
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
