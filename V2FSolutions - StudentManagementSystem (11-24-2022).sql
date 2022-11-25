81fffc65cff35373d4af132685ef60e3831df39f
TaigaAisaka15 <119018584+TaigaAisaka15@users.noreply.github.com>
2022-11-25 08:20:57 +0800

    Update V2FSolutions - StudentManagementSystem (11-24-2022).sql

--- V2FSolutions - StudentManagementSystem (11-24-2022).sql
+++ V2FSolutions - StudentManagementSystem (11-24-2022).sql
@@ -3,7 +3,7 @@
 -- Server version:               10.4.21-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
 -- HeidiSQL Version:             11.3.0.6295
--- Pungak V2 hahahaha!
+-- Pungak V3 hahahaha!
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
